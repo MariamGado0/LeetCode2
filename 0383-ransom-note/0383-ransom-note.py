bd9e0172698b6672c53e2aa8e5e17f8e0b3d8482
Mariam Gado <74381393+MariamGado0@users.noreply.github.com>
2025-09-01 16:32:14 +0300

    Time: 15 ms (75.11%), Space: 18.1 MB (35.12%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+        magazine_count = Counter(magazine)
+
+        for ch in ransomNote:
+            if magazine_count[ch] > 0:
+                magazine_count[ch] -= 1
+            else:
+                return False
+        return True 
+        
